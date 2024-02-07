0556addf0ac250c9a4b370164a1ffe6f1f9d5820
Jonatas <djonatas31@gmail.com>
2024-02-06 22:07:18 -0300

    .

---
+++ 05_Fundamentos_Adicionais/02ProtocoloHTTP/http.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// HTTP É UM PROTOCOLO DE COMUNICAÇÃO - BASE DA COMUNICAÇÃO WEB
+
+	// CLIENTE (FAZ REQUISIÇÃO) - SERVIDOR (PROCESSA REQUISIÇÃO E ENVIA RESPOSTA)
+
+	// Request - Response
+
+	// Rotas
+	// URI - Identificador do Recurso
+	// Método - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
