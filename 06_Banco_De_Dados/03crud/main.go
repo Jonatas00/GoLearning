64d57aadcacd02f9c14d998e2d855d8b494ef6d6
Jonatas <djonatas31@gmail.com>
2024-02-14 23:43:21 -0300

    Feat: Criando rotas

--- 06_Banco_De_Dados/03crud/main.go
+++ 06_Banco_De_Dados/03crud/main.go
@@ -14,6 +14,8 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuario", servidor.BuscarUSuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUSuario).Methods(http.MethodGet)
 
 	fmt.Println("Escutando na porta 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
