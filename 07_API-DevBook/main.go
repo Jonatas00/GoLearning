5db90c5868dd79fbb76e7813a8dbf6a9711d8923
Jonatas <djonatas31@gmail.com>
2024-02-17 14:43:42 -0300

    fix:  add new print with the api port

--- 07_API-DevBook/main.go
+++ 07_API-DevBook/main.go
@@ -10,10 +10,8 @@ import (
 
 func main() {
 	config.Carregar()
-	
-	fmt.Println("Rodando API!")
-
 	r := routes.Gerar()
 
+	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
