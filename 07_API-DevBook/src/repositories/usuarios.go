a03a9cbe55ba5979704d7b7aab6ae9d38a05591d
Jonatas <djonatas31@gmail.com>
2024-02-27 16:29:24 -0300

    feat: add update user in repositorie

--- 07_API-DevBook/src/repositories/usuarios.go
+++ 07_API-DevBook/src/repositories/usuarios.go
@@ -89,3 +89,17 @@ func (repositorio usuarios) BuscarPorID(ID uint64) (models.Usuario, error) {
 	}
 		return usuario, nil
 }
+
+func (repositiorio usuarios) Atualizar(ID uint64, usuario models.Usuario) error {
+	statement, erro := repositiorio.db.Prepare("update usuarios set nome = ?, nick = ?, email = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
