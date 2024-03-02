587bc98a5e31f85139ad9d3b250f9166b0b39016
Jonatas <djonatas31@gmail.com>
2024-03-01 23:57:46 -0300

    feat: create login

---
+++ 07_API-DevBook/src/controllers/login.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDeUsuarios(db)
+
+	
+}
