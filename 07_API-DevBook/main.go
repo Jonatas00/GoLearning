cf0dd2245b6e45df206b34a5a3925a7c24821476
jonatas00 <djonatas31@gmail.com>
2024-05-11 14:26:21 -0300

    git test

--- 07_API-DevBook/main.go
+++ 07_API-DevBook/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	config.Carregar()
 	r := routes.Gerar()
-
+	print("git test")
 	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
