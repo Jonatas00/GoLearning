db731c2bcbf75bc2c715ad69b2c369e0f1e08747
jonatas <djonatas31@gmail.com>
2024-05-19 19:12:20 -0300

    add Busca de Publicacoes

--- 07_API-DevBook/src/controllers/publicacoes.go
+++ 07_API-DevBook/src/controllers/publicacoes.go
@@ -81,7 +81,26 @@ func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := auth.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.Buscar(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	respostas.JSON(w, http.StatusOK, publicacoes)
 }
 
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
