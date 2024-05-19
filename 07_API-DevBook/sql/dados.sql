90814a973f047a827f2ef6f654f6fcc3c2e19526
jonatas <djonatas31@gmail.com>
2024-05-19 18:26:53 -0300

    add publicacoes  template

--- 07_API-DevBook/sql/dados.sql
+++ 07_API-DevBook/sql/dados.sql
@@ -1,11 +1,17 @@
-insert into usuarios (nome, nick, email, senha)
-values
+INSERT INTO usuarios (nome, nick, email, senha)
+VALUES
 ("Usuário 1", "usuario_1", "usuario1@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario1
 ("Usuário 2", "usuario_2", "usuario2@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"), -- usuario2
 ("Usuário 3", "usuario_3", "usuario3@gmail.com", "$2a$10$0iGYlKCAYTyJV/vC6nLGgeWFwD6AhSkWLsVRO/.M4lNK8OtIkfggy"); -- usuario3
 
-insert into seguidores(usuario_id, seguidor_id)
-values
+INSERT INTO seguidores(usuario_id, seguidor_id)
+VALUES
 (1, 2),
 (3, 1),
 (1, 3);
+
+INSERT INTO publicacoes(titulo, conteudo, autor_id)
+VALUES
+("Publicacao do Usuário 1", "Conteúdo da pub do usuário 1", 1),
+("Publicacao do Usuário 2", "Conteúdo da pub do usuário 2", 2),
+("Publicacao do Usuário 3", "Conteúdo da pub do usuário 3", 3);
