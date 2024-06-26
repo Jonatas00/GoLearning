cc4bea92f9a46f7d31acee22cff01fcc99f971e6
jonatas <djonatas31@gmail.com>
2024-06-26 20:43:35 -0300

    refactor config vars

--- 07_API-DevBook/src/config/config.go
+++ 07_API-DevBook/src/config/config.go
@@ -10,9 +10,9 @@ import (
 )
 
 var (
-	StringConexaoBanco = ""
-	Porta              = 0
-	SecretKey []byte
+	StringConexaoBanco string
+	Porta              int
+	SecretKey          []byte
 )
 
 func Carregar() {
@@ -30,7 +30,7 @@ func Carregar() {
 	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&&parseTime=True&&loc=Local",
 		os.Getenv("DB_USUARIO"),
 		os.Getenv("DB_SENHA"),
-		os.Getenv("DB_NOME"),	
+		os.Getenv("DB_NOME"),
 	)
 
 	SecretKey = []byte(os.Getenv("SECRET_KEY"))
