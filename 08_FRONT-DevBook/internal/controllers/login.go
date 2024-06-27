337674948905b47d4617f0915d6c1a7c311c9b70
jonatas <djonatas31@gmail.com>
2024-06-26 22:06:20 -0300

    configure env variables

--- 08_FRONT-DevBook/internal/controllers/login.go
+++ 08_FRONT-DevBook/internal/controllers/login.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/internal/config"
 	"webapp/internal/models"
 	"webapp/internal/respostas"
 )
@@ -20,7 +22,8 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
+	url := fmt.Sprintf("%s/login", config.APIURL)
+	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
--- 08_FRONT-DevBook/main.go
+++ 08_FRONT-DevBook/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/internal/config"
 	"webapp/internal/router"
 	"webapp/internal/utils"
 )
 
 func main() {
+	config.Carregar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
-	fmt.Println("Rodando na porta 3000")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Printf("Escutando na porta %d", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
