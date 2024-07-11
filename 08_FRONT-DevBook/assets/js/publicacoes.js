fecb3ed2196a18f9b6692b8c6e064db099aefd24
jonatas <djonatas31@gmail.com>
2024-07-10 23:59:42 -0300

    curtir publicacao

--- 08_FRONT-DevBook/assets/js/publicacoes.js
+++ 08_FRONT-DevBook/assets/js/publicacoes.js
@@ -1,4 +1,5 @@
-$(`#nova-publicacao`).on(`submit`, criarPublicacao)
+$('#nova-publicacao').on('submit', criarPublicacao);
+$('.curtir-publicacao').on('click', curtirPublicacao);
 
 function criarPublicacao(e) {
   e.preventDefault();
@@ -14,5 +15,27 @@ function criarPublicacao(e) {
     window.location = "/home";
   }).fail(() => {
     alert("Erro ao criar publicação!");
+  })
+}
+
+function curtirPublicacao(e) {
+  e.preventDefault();
+
+  const elementoClicado = $(e.target);
+  const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+  elementoClicado.prop("disabled", true);
+  $.ajax({
+    url: `/publicacoes/${publicacaoId}/curtir`,
+    method: "POST"
+  }).done(() => {
+    const contadorDeCurtidas = elementoClicado.next("span");
+    const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+    contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+  }).fail(() => {
+    alert("erro ao curtir a publicacao!")
+  }).always(() => {
+    elementoClicado.prop("disabled", false);
   })
 }
