cdd2cfdd26f8a3144ed97a70fa57ea21a895a334
jonatas <djonatas31@gmail.com>
2024-07-24 00:24:35 -0300

    add descurtir publicacao

--- 08_FRONT-DevBook/assets/js/publicacoes.js
+++ 08_FRONT-DevBook/assets/js/publicacoes.js
@@ -1,5 +1,6 @@
-$('#nova-publicacao').on('submit', criarPublicacao);
-$('.curtir-publicacao').on('click', curtirPublicacao);
+$("#nova-publicacao").on("submit", criarPublicacao);
+$(document).on("click", ".curtir-publicacao", curtirPublicacao);
+$(document).on("click", ".descurtir-publicacao", descurtirPublicacao);
 
 function criarPublicacao(e) {
   e.preventDefault();
@@ -22,7 +23,7 @@ function curtirPublicacao(e) {
   e.preventDefault();
 
   const elementoClicado = $(e.target);
-  const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+  const publicacaoId = elementoClicado.closest("div").data("publicacao-id");
 
   elementoClicado.prop("disabled", true);
   $.ajax({
@@ -33,8 +34,38 @@ function curtirPublicacao(e) {
     const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
 
     contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+    elementoClicado.addClass("descurtir-publicacao");
+    elementoClicado.addClass("text-danger");
+    elementoClicado.removeClass("curtir-publicacao");
   }).fail(() => {
     alert("erro ao curtir a publicacao!")
+  }).always(() => {
+    elementoClicado.prop("disabled", false);
+  })
+}
+
+function descurtirPublicacao(e) {
+  e.preventDefault();
+
+  const elementoClicado = $(e.target);
+  const publicacaoId = elementoClicado.closest("div").data("publicacao-id");
+
+  elementoClicado.prop("disabled", true);
+  $.ajax({
+    url: `/publicacoes/${publicacaoId}/descurtir`,
+    method: "POST"
+  }).done(() => {
+    const contadorDeCurtidas = elementoClicado.next("span");
+    const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+    contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+    elementoClicado.removeClass("descurtir-publicacao");
+    elementoClicado.removeClass("text-danger");
+    elementoClicado.addClass("curtir-publicacao");
+  }).fail(() => {
+    alert("erro ao descurtir a publicacao!")
   }).always(() => {
     elementoClicado.prop("disabled", false);
   })
